97c61a7ec44bf909917e37e6fff93f8b4e9db99c
Dpavaman <67939138+Dpavaman@users.noreply.github.com>
2020-10-28 16:37:49 +0530

    Updates

---
+++ precache-manifest.a45029351a5ab78108cbf50dff7d97c7.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e1dc6f9294f034d98b3e19638f82ca",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c1108a4317de17f0c0c",
+    "url": "/portfolio/static/css/main.c02b20da.chunk.css"
+  },
+  {
+    "revision": "099f91d6b4fd0aa6df43",
+    "url": "/portfolio/static/js/2.3a907508.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/portfolio/static/js/2.3a907508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c1108a4317de17f0c0c",
+    "url": "/portfolio/static/js/main.bd3b2f4b.chunk.js"
+  },
+  {
+    "revision": "db39b0e1f2ee7d3a5637",
+    "url": "/portfolio/static/js/runtime-main.ad3b6095.js"
+  },
+  {
+    "revision": "5527e10fb308d9ad33f61b3476bcdd6e",
+    "url": "/portfolio/static/media/Calculator.5527e10f.png"
+  },
+  {
+    "revision": "b6b22dd09c81536784632d80c61cf6f3",
+    "url": "/portfolio/static/media/Covid-dashboard.b6b22dd0.png"
+  },
+  {
+    "revision": "f1bcf802509af1f7d399c3e72faa8ab0",
+    "url": "/portfolio/static/media/HTMLIcon.f1bcf802.svg"
+  },
+  {
+    "revision": "84c52e2b1ce92701de63bfa0d660af14",
+    "url": "/portfolio/static/media/Hangman-game.84c52e2b.png"
+  },
+  {
+    "revision": "3616088a46760530288d4618f416b24b",
+    "url": "/portfolio/static/media/Top-stories-NEWYORKTIMES-API.3616088a.png"
+  },
+  {
+    "revision": "9d45e9df95ba6bef639d71c972e835f6",
+    "url": "/portfolio/static/media/bootstrap-4.9d45e9df.svg"
+  },
+  {
+    "revision": "d6f2b958272a0511df6c77e704f23fdb",
+    "url": "/portfolio/static/media/carousel-1.d6f2b958.jpg"
+  },
+  {
+    "revision": "3ab5bccfc6acf687c404e76c28505f96",
+    "url": "/portfolio/static/media/carousel-2.3ab5bccf.jpg"
+  },
+  {
+    "revision": "199f66142fa79b4a3cd5130480b2c14b",
+    "url": "/portfolio/static/media/carousel-3.199f6614.jpg"
+  },
+  {
+    "revision": "585ac15de7d8b1390881ba43b3ddcfe2",
+    "url": "/portfolio/static/media/css3.585ac15d.svg"
+  },
+  {
+    "revision": "0fb30b49da79da6979871081a23bfd53",
+    "url": "/portfolio/static/media/expressjs.0fb30b49.svg"
+  },
+  {
+    "revision": "c25ae9736ce8713786bb675e2ca18506",
+    "url": "/portfolio/static/media/github-icon.c25ae973.svg"
+  },
+  {
+    "revision": "4d9f9e02521682c61b7f0f611619656b",
+    "url": "/portfolio/static/media/heroku.4d9f9e02.svg"
+  },
+  {
+    "revision": "018c88ab601df52e95372fa6491058b2",
+    "url": "/portfolio/static/media/javascript.018c88ab.svg"
+  },
+  {
+    "revision": "cbb67654626c7a2e1a5e7c01bf41684a",
+    "url": "/portfolio/static/media/material-ui.cbb67654.svg"
+  },
+  {
+    "revision": "76757d113fa0bcc1fdbad2cae5b1293e",
+    "url": "/portfolio/static/media/mentor-mentee.76757d11.png"
+  },
+  {
+    "revision": "cac6cc667486058c760f554b4f762d7c",
+    "url": "/portfolio/static/media/mongodb.cac6cc66.svg"
+  },
+  {
+    "revision": "7847e98ebc6ef437aef0212ec688b341",
+    "url": "/portfolio/static/media/myportrait.7847e98e.jpg"
+  },
+  {
+    "revision": "4b94e2f27420da3b3faa293e10ee57fc",
+    "url": "/portfolio/static/media/netlify.4b94e2f2.svg"
+  },
+  {
+    "revision": "fc1ad7b67edf22fce991e249c5f88b4a",
+    "url": "/portfolio/static/media/nodejs.fc1ad7b6.svg"
+  },
+  {
+    "revision": "fe67b7c833fe23158b06a070338b4c8a",
+    "url": "/portfolio/static/media/npm.fe67b7c8.svg"
+  },
+  {
+    "revision": "b51f3108d1782020f022d8e1db5cc271",
+    "url": "/portfolio/static/media/parallax-background.b51f3108.jpg"
+  },
+  {
+    "revision": "9aaeded9e6fb06f2b668b3945d5a0b18",
+    "url": "/portfolio/static/media/project-background.9aaeded9.jpg"
+  },
+  {
+    "revision": "f87688d67e6af214dd57cc17cfe523f2",
+    "url": "/portfolio/static/media/react.f87688d6.svg"
+  },
+  {
+    "revision": "9a62b04a4f08c8e762307c7d2519caf0",
+    "url": "/portfolio/static/media/technologies-background.9a62b04a.jpg"
+  },
+  {
+    "revision": "0022f64d5086bcc1f7dec16e239aea7a",
+    "url": "/portfolio/static/media/weatherAPI.0022f64d.png"
+  }
+]);
