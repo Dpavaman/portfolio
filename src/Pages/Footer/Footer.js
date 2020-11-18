afbe4743d0f0cef0e6b4d6203889c84265669e31
Dpavaman <67939138+Dpavaman@users.noreply.github.com>
2020-11-18 10:23:11 +0530

    bug fixed after adding new project

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -5,6 +5,7 @@ const FooterComponent = () => {
     return (
         <div>
             <div className="py-2 text-center footer-style">
+                <h4> This portfolio is indeed one of my projects..  </h4>
                 <a className='linkToSC' href="https://github.com/Dpavaman/portfolio" target='_blank' rel='noopener noreferrer'>
                     <h5>View Source code of this webpage</h5>
                 </a>
